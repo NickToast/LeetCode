b2565ac6ce9f6081b5303d7acf7ff37601b7151c
NickToast <nicholastangsouvanh@gmail.com>
2023-12-16 01:28:17 -0800

    can place flowers variation solution js

--- LeetCode 75/Array String/605. Can Place Flowers/CanPlaceFlowers.js
+++ LeetCode 75/Array String/605. Can Place Flowers/CanPlaceFlowers.js
@@ -12,6 +12,24 @@ flowerbed2 = [1,0,0,0,1], n2 = 2;
 output2 = false;
 
 // Loop through flowerbed
+// Check if i = 0, if so, check left and right, if != 0, continue to next flower spot
+// Check if left and right is == 0, if so then lower n by 1 and continue
+// return true or false (if n <= 0 then the flowers fit)
+
+var canPlaceFlowersVariation = function (flowerbed, n) {
+    let i = 0;
+    
+    while (i < flowerbed.length && n != 0) {
+        if (flowerbed[i] === 0 && flowerbed[i-1] !== 1 && flowerbed[i+1] !== 1) {
+            n--;
+            i++;
+        }
+        i++;
+    }
+    return n === 0;
+}
+
+// Loop through flowerbed
 // 
 // Return true or false
 
@@ -24,6 +42,8 @@ var canPlaceFlowers = function(flowerbed, n) {
     }
     return n<= 0;
 };
+
+
 
 console.log("Output:", canPlaceFlowers(flowerbed1, n1));
 console.log("Expected Output:", output1);

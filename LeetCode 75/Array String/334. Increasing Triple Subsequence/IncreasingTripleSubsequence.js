c0bc697c93676ecd185e64f70e0c3f7285edbc7d
NickToast <nicholastangsouvanh@gmail.com>
2023-12-18 23:58:33 -0800

    increasing triple subsequence start

---
+++ LeetCode 75/Array String/334. Increasing Triple Subsequence/IncreasingTripleSubsequence.js
@@ -0,0 +1,19 @@
+// Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
+
+// Example 1
+nums1 = [1,2,3,4,5];
+output1 = true;
+
+// Example 2
+nums2 = [5,4,3,2,1];
+output2 = false;
+
+// Example 3
+nums3 = [2,1,5,0,4,6];
+output3 = true;
+
+var increasingTriplet = function(nums) {
+    for (let i = 0; i < nums.length - 2; i++) {
+        
+    }
+}

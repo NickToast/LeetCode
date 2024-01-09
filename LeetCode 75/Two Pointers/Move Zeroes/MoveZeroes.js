a74d7b8204480e3fff34fbdfa80864748f645245
NickToast <nicholastangsouvanh@gmail.com>
2024-01-08 23:58:57 -0800

    move zeroes update

---
+++ LeetCode 75/Two Pointers/Move Zeroes/MoveZeroes.js
@@ -0,0 +1,20 @@
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Note that you must do this in-place without making a copy of the array.
+
+nums1 = [0,1,0,2,3,12];
+output1 = [1,3,12,0,0];
+
+nums2 = [0];
+output2 = [0];
+
+var moveZeroes = function(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        if (nums[left] === 0) {
+            
+        }
+    }
+};

4332062b7e12fb0920d04a1d4b9f1a1e5f06ba9a
NickToast <nicholastangsouvanh@gmail.com>
2024-01-16 19:25:32 -0800

    max num of vowels in substring of given length js solution

--- LeetCode 75/Sliding Window/1456. Maximum Number of Vowels in a Substring of Given Length/MaxNumOfVowelsInSubstringOfGivenLength.js
+++ LeetCode 75/Sliding Window/1456. Maximum Number of Vowels in a Substring of Given Length/MaxNumOfVowelsInSubstringOfGivenLength.js
@@ -12,11 +12,35 @@ let s3 = "leetcode", k3 = 3;
 let output3 = 2; 
 
 var maxVowels = function(s, k) {
-    let count = 0;
-    while (i < s.length - k){
-        
+    // New vowel set
+    let vowelSet = new Set(['a', 'e', 'i', 'o', 'u']);
+
+    // Max variable for max num of vowel letters in any substring
+    let maxVowels = 0;
+    let vowels = 0;
+    let windowStart = 0;
+
+    for(let windowEnd = 0; windowEnd < s.length; windowEnd++){
+        // Check if length is greater than k, then increment windowStart so we only work in a substring of the length k
+        if (windowEnd - windowStart + 1 > k) {
+            // Check if s[windowStart], the beginning of the window value, contains a vowel, then decrease the vowel counter for the substring
+            if (vowelSet.has(s[windowStart])) {
+                vowels--;
+            }
+            windowStart++;
+        }
+        // If above conditional does not go through, then that means we now have a substring of length k
+        // Now we want to increase our vowels counter for each vowel in the substring
+        if (vowelSet.has(s[windowEnd])) { // If s[windowEnd] is in the vowelSet that we created then do this
+            vowels++; // Increase our vowels counter
+            maxVowels = Math.max(maxVowels, vowels);
+        }
     }
+    return maxVowels;
 };
+
+// Time complexity is O(n) where n is the length of the string
+// Space complexity is O(1) constant
 
 console.log("Output:", maxVowels(s1,k1));
 console.log("Expected:", output1);

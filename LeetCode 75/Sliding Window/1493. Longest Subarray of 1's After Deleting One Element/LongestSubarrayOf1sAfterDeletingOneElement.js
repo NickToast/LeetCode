3a05db3ffe05c0dd9f9bea035097952063f61741
NickToast <nicholastangsouvanh@gmail.com>
2024-01-18 17:03:17 -0800

    longest subarray of 1s after deleting one element js solution

---
+++ LeetCode 75/Sliding Window/1493. Longest Subarray of 1's After Deleting One Element/LongestSubarrayOf1sAfterDeletingOneElement.js
@@ -0,0 +1,44 @@
+// Given a binary array nums, you should delete one element from it.
+
+// Return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.
+
+let nums1 = [1,1,0,1], output1 = 3;
+let nums2 = [0,1,1,1,0,1,1,0,1], output2 = 5;
+let nums3 = [1,1,1], output3 = 2;
+
+var longestSubarray = function(nums) {
+    let windowStart = 0;
+    let k = 1; // k represents the maximum number of elements we can delete
+    let maxLength = 0;
+
+    // Loop through array
+    for (let windowEnd = 0; windowEnd < nums.length; windowEnd++) {
+        // If right window equals 0, decrement k
+        if (nums[windowEnd] === 0) {
+            k--;
+        }
+        // If k is less than 0, that means we've deleted 1 too many elements, so we keep incrementing the left window until we get to a 0, so we can increment k again
+        if (k < 0) {
+            if(nums[windowStart] === 0) {
+                k++;
+            }
+            windowStart++;
+        }
+        // Updates the maximum length each time the loop goes through
+        maxLength = Math.max(maxLength, windowEnd - windowStart);
+    };
+    return maxLength;
+};
+
+console.log("Output:", longestSubarray(nums1));
+console.log("Expected:", output1);
+console.log("================");
+
+console.log("Output:", longestSubarray(nums2));
+console.log("Expected:", output2);
+console.log("================");
+
+console.log("Output:", longestSubarray(nums3));
+console.log("Expected:", output3);
+console.log("================");
+

68e011e31ff2e9b6e2b3b462d7cc6e1320f3e4ed
NickToast <nicholastangsouvanh@gmail.com>
2024-01-29 22:38:33 -0800

    determine if two strings are close js solution

--- LeetCode 75/Hash Map and Set/1657. Determine If Two Strings Are Close/DetermineIfTwoStringsAreClose.js
+++ LeetCode 75/Hash Map and Set/1657. Determine If Two Strings Are Close/DetermineIfTwoStringsAreClose.js
@@ -8,6 +8,43 @@
 
 // Given two strings, word1 and word2, return true if word1 and word2 are close, and false otherwise.
 
+let word1 = "abc", word2 = "bca", word3 = "a", word4 = "aa", word5 = "cabbba", word6 = "abbccc";
+let ans1 = true, ans2 = false, ans3 = true;
+
 var closeString = function(word1, word2) {
     // 
+    if (word1.length !== word2.length) return false;
+
+    let arr1 = new Array(26).fill(0);
+    let arr2 = new Array(26).fill(0);
+
+    for (let i = 0; i < word1.length; i++) {
+        arr1[word1[i].charCodeAt() - 'a'.charCodeAt()]++;
+        arr2[word1[i].charCodeAt() - 'a'.charCodeAt()]++;
+    }
+
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] && !arr2[i] || arr2[i] && !arr1[i]) return false;
+    }
+
+    arr1.sort((a,b) => b-a);
+    arr2.sort((a,b) => b-a);
+
+    let j = 0;
+    while (j < arr1.length) {
+        if (arr1[j] != arr2[j]) return false;
+        if (arr1[j] <= 0 && arr2[j] <= 0) break;
+        j++;
+    }
+    return true;
 };
+
+console.log("Output:", closeString(word1,word2));
+console.log("Expected:", ans1);
+console.log("-------------");
+console.log("Output:", closeString(word3,word4));
+console.log("Expected:", ans2);
+console.log("-------------");
+console.log("Output:", closeString(word5,word6));
+console.log("Expected:", ans3);
+console.log("-------------");
